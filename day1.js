4a2ee229a5f63f421dd76a3b6840c25a3f3b8512
Uwumugisha <uwumugishachersana45@gmail.com>
2023-11-21 16:34:03 +0200

    operators in JavaScript

---
+++ day1.js
@@ -0,0 +1,22 @@
+//addition in JavaScript
+var a = 303;
+var b = 402;
+const sum = a + b;
+console.log(sum);
+// multiplication in Javascript
+const product = a * b;
+console.log(product);
+//substration in Javascript
+const dif = b - a;
+console.log(dif);
+//division in Javascript
+const quotient= b / a;
+console.log(quotient);
+var n = "my name is ";
+var m = "chersana";
+var myName = n + m;
+console.log(myName);
+var myName = "my name is ";
+myName += "chersana";
+console.log(myName);
+ 
