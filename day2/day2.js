4f814ce5be48fce0a4d929fd3af64bebfa9e840b
Uwumugisha <uwumugishachersana45@gmail.com>
2023-11-22 16:43:14 +0200

    comparison in javascript

---
+++ day2/day2.js
@@ -0,0 +1,10 @@
+//boolean in javascript
+let isOnSale = true;
+console.log(isOnSale);
+isOnSale = false;
+console.log(isOnSale);
+const num1 = 5;
+const num2 = 6;
+console.log(num1 > num2);
+console.log(num1 < num2);
+console,log(num1 <= num2);
