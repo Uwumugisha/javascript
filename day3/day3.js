941022ab70e605e98bdd2e31e73a10763f306728
Uwumugisha <uwumugishachersana45@gmail.com>
2023-11-23 16:42:42 +0200

    trying function and conditionals in JavaScript

---
+++ day3/day3.js
@@ -0,0 +1,38 @@
+// string in javascript
+const textOne = 'hello';
+const textTwo = 'world';
+const combined = textOne + ' ' + textTwo;
+console.log(combined);
+// conditionals in javascript
+let temperature = 75;
+console.log(`the temperature is ${temperature}`);
+
+if (temperature > 70) {
+    console.log("it's warm outside");
+}
+
+let myAge = 40;
+console.log(`my age is ${myAge}`);
+ if (myAge <= 30){
+    console.log('young lady');
+ }
+ //comparing to variables
+let a = '4';
+let b = '4';
+if(a === b){
+    console.log("we");
+}
+else{
+    console.log("me");
+}
+//using functions in javascript
+let num1 = "chersana";
+let num2 = "1234";
+function functionName(num1,num2){
+    if(num1 && num2 == true){
+        console.log("welcome to our websibe");
+    }
+    else{
+        console.log("Please fill your cridentials");
+    }
+}
